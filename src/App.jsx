249a159193f44dc1d469294f4d5dc1f4ad4a29e8
Vinícius Dias <vyvy292004@gmail.com>
2026-02-06 17:21:39 -0300

    feat: adds Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Routes } from 'react-router-dom'
-import { Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
 function App() {
   return <div className='app'>
+    <Navbar />
     <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='/auth' element={<Auth />}></Route>
       <Route path='/checkout' element={<Checkout />}></Route>
     </Routes>
-  </div>
+  </div >
 }
 
 export default App
